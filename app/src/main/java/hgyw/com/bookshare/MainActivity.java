f5ddb04f422ca0332e68d6ffcc0666f206a58741
haimgr <haim763@gmail.com>
2016-05-04 16:07:05 +0300

    תיקון ID ומעבר לאנדרואיד

--- app/src/main/java/hgyw/com/bookshare/MainActivity.java
+++ app/src/main/java/hgyw/com/bookshare/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import hgyw.com.bookshare.entities.Book;
-import hgyw.com.bookshare.entities.Entity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Book book = new Book();
 
-        Entity e = new Entity() {{ setId(4636); }};
     }
+
+
 }
--- javamodule/src/main/java/hgyw/com/bookshare/entities/Rating.java
+++
@@ -1,8 +0,0 @@
-package hgyw.com.bookshare.entities;
-
-/**
- * Created by Yoni on 3/25/2016.
- */
-public enum Rating {
-    EMPTY ,POOR, BAD, MEDIUM, GOOD, EXCELLENT
-}
